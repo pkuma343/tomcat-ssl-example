805ce9afdbc1755a0cdb2fb3ce8d2604d10d3f37
Pankaj Kumar (C360-EPMP) <panks36305@gmail.com>
2019-10-20 09:11:29 +0530

    eigth commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,8 +11,8 @@ node {
 		// This is where our artifacts come from
 		rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
 		//def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'install'
-		def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'install -Drevision=RELEASE-SNAPSHOT'
-		def buildInfo1 = rtMaven.run pom: 'pom.xml', goals: 'install -Drevision=0.0.2'
+		def buildInfo = rtMaven.run pom: 'pom.xml', goals: "install -Drevision=RELEASE-SNAPSHOT"
+		def buildInfo1 = rtMaven.run pom: 'pom.xml', goals: "install -Drevision=0.0.2"
 		server.publishBuildInfo buildInfo
 		server.publishBuildInfo buildInfo1
 	}
