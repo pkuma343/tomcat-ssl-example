19cebf4a3c3e6dd210eb674cfbc7c9acc6be9eff
Pankaj Kumar (C360-EPMP) <panks36305@gmail.com>
2019-10-20 08:45:44 +0530

    sixth commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,7 +12,7 @@ node {
 		rtMaven.resolver releaseRepo:'libs-release', snapshotRepo:'libs-snapshot', server: server
 		//def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'install'
 		def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'install -Drevision=RELEASE-SNAPSHOT'
-		def buildInfo1 = rtMaven.run pom: 'pom.xml', goals: 'install -Drevision=0.0.1'
+		def buildInfo1 = rtMaven.run pom: 'pom.xml', goals: 'install -Drevision=0.0.2'
 		server.publishBuildInfo buildInfo
 		server.publishBuildInfo buildInfo1
 	}
